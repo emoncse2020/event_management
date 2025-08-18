f5697bf5daa927cda246845112a729e458ea7886
emoncse2020 <emon.cse.2020@gmail.com>
2025-08-18 23:01:00 +0600

    postcss added

--- events/models.py
+++ events/models.py
@@ -2,16 +2,36 @@ from django.db import models
 
 # Create your models here.
 
+class Category (models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
 
-# class Event(models.Model):
+    def __str__(self):
+        return f"{self.name}"
 
-#     name = models.CharField(max_length=100)
+class Event(models.Model):
+
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='events')
+
+    def __str__(self):
+        return f"{self.name} | {self.date}"
+    
+class Participant(models.Model):
+
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True )
+    events = models.ManyToManyField(Event, related_name='participants', blank=True)
+
+
+
+    def __str__(self):
+        return f"{self.name} | {self.email}"
     
 
-    
-
-    
-
-#     def __str__(self):
-#         return self.name
+   
 
